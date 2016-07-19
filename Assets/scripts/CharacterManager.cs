0db030cacdcb3115f4e2c135d175a8c3ff1365f1
Jeff Knox <jd.knox@gmail.com>
2016-07-18 19:46:00 -0700

    fix gvr upgrade issues

--- Assets/scripts/CharacterManager.cs
+++ Assets/scripts/CharacterManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class characterManager : MonoBehaviour {
+public class CharacterManager : MonoBehaviour {
 
   public float maxPlayerSpeed;
 
